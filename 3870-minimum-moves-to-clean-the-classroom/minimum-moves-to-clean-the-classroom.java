3f0c1ddf2d1a7ed4962c222e2c72ea6245c5f7f6
sastiga03 <sastiga03@gmail.com>
2026-09-07 21:44:00 +0530

    Time: 120 ms (61.51%) | Memory: 60.4 MB (58.33%) - LeetSync

--- 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.java
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.java
@@ -1,98 +1,151 @@
+import java.util.*;
+
 class Solution {
+
+    class State {
+        int r, c, energy, mask, moves;
+
+        State(int r, int c, int energy, int mask, int moves) {
+            this.r = r;
+            this.c = c;
+            this.energy = energy;
+            this.mask = mask;
+            this.moves = moves;
+        }
+    }
+
     public int minMoves(String[] classroom, int energy) {
+
         int m = classroom.length;
         int n = classroom[0].length();
 
-        int[][] id = new int[m][n];
+        char[][] mat = new char[m][n];
 
-        for (int r = 0; r < m; r++) {
-            java.util.Arrays.fill(id[r], -1);
+        int sr = 0, sc = 0;
+        int litter = 0;
+
+        int[][] index = new int[m][n];
+
+        for (int[] row : index) {
+            Arrays.fill(row, -1);
         }
 
-        int k = 0;
-        int sr = 0, sc = 0;
+        // Create matrix + find S + number L
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (classroom[r].charAt(c) == 'S') {
-                    sr = r;
-                    sc = c;
-                } else if (classroom[r].charAt(c) == 'L') {
-                    id[r][c] = k++;
+                mat[i][j] = classroom[i].charAt(j);
+
+                if (mat[i][j] == 'S') {
+                    sr = i;
+                    sc = j;
+                }
+
+                if (mat[i][j] == 'L') {
+                    index[i][j] = litter;
+                    litter++;
                 }
             }
         }
 
-        if (k == 0) return 0;
+        // No litter
+        if (litter == 0)
+            return 0;
 
-        int totalMask = (1 << k) - 1;
+        // Example: 3 litter -> 111
+        int all = (1 << litter) - 1;
 
-        int[][][] best = new int[m][n][1 << k];
+        /*
+            visited[r][c][mask]
 
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                java.util.Arrays.fill(best[r][c], -1);
+            Stores maximum energy with which
+            we have reached this state.
+        */
+        int[][][] visited = new int[m][n][1 << litter];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                Arrays.fill(visited[i][j], -1);
             }
         }
 
-        class State {
-            int r, c, mask, e, moves;
+        Queue<State> q = new LinkedList<>();
 
-            State(int r, int c, int mask, int e, int moves) {
-                this.r = r;
-                this.c = c;
-                this.mask = mask;
-                this.e = e;
-                this.moves = moves;
+        // Start
+        q.offer(new State(sr, sc, energy, 0, 0));
+
+        visited[sr][sc][0] = energy;
+
+        int[][] dir = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+
+        while (!q.isEmpty()) {
+
+            State cur = q.poll();
+
+            // All litter collected
+            if (cur.mask == all) {
+                return cur.moves;
             }
-        }
 
-        java.util.ArrayDeque<State> queue = new java.util.ArrayDeque<>();
+            for (int[] d : dir) {
 
-        best[sr][sc][0] = energy;
-        queue.offer(new State(sr, sc, 0, energy, 0));
+                int nr = cur.r + d[0];
+                int nc = cur.c + d[1];
 
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-
-        while (!queue.isEmpty()) {
-            State cur = queue.poll();
-
-            for (int d = 0; d < 4; d++) {
-                int nr = cur.r + dr[d];
-                int nc = cur.c + dc[d];
-
+                // Outside
                 if (nr < 0 || nr >= m || nc < 0 || nc >= n)
                     continue;
 
-                if (classroom[nr].charAt(nc) == 'X')
+                // Obstacle
+                if (mat[nr][nc] == 'X')
                     continue;
 
-                int ne = cur.e - 1;
-
-                if (ne < 0)
+                // Cannot move without energy
+                if (cur.energy == 0)
                     continue;
 
-                int nmask = cur.mask;
+                // Spend 1 energy
+                int newEnergy = cur.energy - 1;
 
-                if (classroom[nr].charAt(nc) == 'R') {
-                    ne = energy;
+                int newMask = cur.mask;
+
+                // Reset
+                if (mat[nr][nc] == 'R') {
+                    newEnergy = energy;
                 }
 
-                if (classroom[nr].charAt(nc) == 'L') {
-                    nmask |= (1 << id[nr][nc]);
+                // Litter
+                if (mat[nr][nc] == 'L') {
+
+                    int id = index[nr][nc];
+
+                    newMask = newMask | (1 << id);
                 }
 
-                if (nmask == totalMask) {
-                    return cur.moves + 1;
-                }
-
-                if (ne <= best[nr][nc][nmask])
+                /*
+                    If we already reached this
+                    position + mask with MORE energy,
+                    this path is useless.
+                */
+                if (visited[nr][nc][newMask] >= newEnergy) {
                     continue;
+                }
 
-                best[nr][nc][nmask] = ne;
+                // This is a better state
+                visited[nr][nc][newMask] = newEnergy;
 
-                queue.offer(new State(nr, nc, nmask, ne, cur.moves + 1));
+                q.offer(new State(
+                    nr,
+                    nc,
+                    newEnergy,
+                    newMask,
+                    cur.moves + 1
+                ));
             }
         }
 
