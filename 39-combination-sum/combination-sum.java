4e374a594a230d0931ac34d48030d055a9b1acaa
sastiga03 <sastiga03@gmail.com>
2026-09-09 09:42:20 +0530

    Time: 2 ms (94.89%) | Memory: 45.7 MB (50.66%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>ans=new ArrayList<>();
+        backtrack(candidates,target,0,new ArrayList<>(),ans);
+        return ans;
+    }
+    public void backtrack(int[]candidates,int target,int start,List<Integer>res,List<List<Integer>>ans){
+        if(target==0){
+            ans.add(new ArrayList<>(res));
+            return;
+        }
+        if(target<0){
+            return;
+        }
+        for(int i=start;i<candidates.length;i++){
+            res.add(candidates[i]);
+            backtrack(candidates,target-candidates[i],i,res,ans);
+            res.remove(res.size()-1);
+        }
+    }
+}
